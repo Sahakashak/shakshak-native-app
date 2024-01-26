bd67878979816228f1a7ff9054752b0e80c6dd10
Arup Basak <email.arupbasak@gmail.com>
2024-01-26 19:20:10 +0530

    setup react native paper

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import HomeScreen from "./screens/HomeScreen";
 import OnbordingScreen from "./screens/OnbordingScreen";
 import OtpScreen from "./screens/OtpScreen";
 import DashboardScreen from "./screens/DashboardScreen";
+import { PaperProvider } from "react-native-paper";
 
 type RootStackParamList = {
   HomeScreen: undefined;
@@ -16,14 +17,16 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="OnboardingScreen" component={OnbordingScreen} />
-        <Stack.Screen name="OtpScreen" component={OtpScreen} />
-        <Stack.Screen name="DashboardScreen" component={DashboardScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="OnboardingScreen" component={OnbordingScreen} />
+          <Stack.Screen name="OtpScreen" component={OtpScreen} />
+          <Stack.Screen name="DashboardScreen" component={DashboardScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
